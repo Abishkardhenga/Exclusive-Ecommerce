39583057f840f85f02b636868558e3f9fff98ff5
Aabiskar <Copywriterayan47@gmail.com>
2023-12-04 01:53:34 +0545

    frontend setup package and routing done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Aboutus from "./Pages/Aboutus/Aboutus";
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
+import Whislist from "./Pages/Whislist/Whislist";
+import Product from "./Pages/Product/Product";
+import Contact from "./Pages/Contact/Contact";
+import Checkout from "./Pages/Checkout/Checkout";
+import Cart from "./Pages/Cart/Cart";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<Aboutus />} />
+      <Route path="/whislist" element={<Whislist />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
