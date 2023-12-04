e31532cb07a815b43040a94cd07f13d8bdfa4202
Aabiskar <Copywriterayan47@gmail.com>
2023-12-04 10:01:25 +0545

    register backend done

--- Backend/Controller/Auth.js
+++ Backend/Controller/Auth.js
@@ -1,15 +1,43 @@
+const owner = require("../Models/Owner");
 const LoginHandler = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    res.status(200).json({ message: "Login SuccessFully ", success: true });
+    const data = owner.find({ email });
+
+    if (!data) {
+      res.status(404).json({ message: "No data found ", success: false });
+    }
+    if (!email && !password) {
+      res
+        .status(403)
+        .json({ message: " Enter all the fields", success: false });
+    } else if (data.password === password) {
+      res
+        .status(200)
+        .json({ message: "Login SuccessFully ", data, success: true });
+    }
   } catch (err) {
     res.status(403).json({ message: "error", err, success: false });
   }
 };
 const RegisterHandler = async (req, res) => {
+  const { email, password, username } = req.body;
+
   try {
-    res.status(200).json({ message: " Register SuccessFully ", success: true });
+    if (!email || !password || !username) {
+      res
+        .status(400)
+        .json({ message: "Enter all the fields ", success: false });
+    } else {
+      const data = await owner.create(req.body);
+      res
+        .status(200)
+        .json({ message: " Register SuccessFully ", data, success: true });
+    }
   } catch (err) {
-    res.status(403).json({ message: "error", err, success: false });
+    res.status(500).json({ message: "error", err, success: false });
+    console.log("this is err", err);
   }
 };
 
--- Backend/index.js
+++ Backend/index.js
@@ -3,9 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const db = require("./utilis/db");
-
+app.use(express.json());
 db();
-
+app.use(require("./Routes/AuthRoutes"));
 app.listen(8000, () => {
   console.log("port started at 8000");
 });
