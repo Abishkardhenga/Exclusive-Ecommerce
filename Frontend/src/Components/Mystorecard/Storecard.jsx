7c450ed23c03987827c99129b963bd3b7b2fd8d0
Aabiskar <Copywriterayan47@gmail.com>
2023-12-11 22:31:36 +0545

    done with delete functionality

--- Frontend/src/Components/Mystorecard/Storecard.jsx
+++ Frontend/src/Components/Mystorecard/Storecard.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./Storecard.module.css";
+import axios from "axios";
+import { UserInfo } from "../../utilis/UseContext/UseContext";
 
 const Storecard = ({ item }) => {
+  let { state, dispatch } = useContext(UserInfo);
+  let deleteapi = "http://localhost:8000/deleteproduct";
+
+  let handleDelete = async (item) => {
+    console.log("this is item", item);
+    console.log("this is item", item._id);
+
+    try {
+      const { data, status } = await axios.delete(`${deleteapi}/${item?._id}`);
+      if (status == 200) {
+        alert("successfully deleted");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.imgWrapper}>
@@ -14,7 +32,14 @@ const Storecard = ({ item }) => {
         <div className={styles.btnWrapper}>
           <button className={styles.editBtn}>edit</button>
 
-          <button className={styles.deleteBtn}>delete</button>
+          <button
+            onClick={() => {
+              handleDelete(item);
+            }}
+            className={styles.deleteBtn}
+          >
+            delete
+          </button>
         </div>
       </div>
     </div>
